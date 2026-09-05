01201e18d8bfbb66463ad9be44ce0c96e4c8ba0b
Uzma <uzma42nosheen@gmail.com>
2026-09-05 13:48:29 +0500

    Deleted Test/Debug Artifacts

--- extractor_test_doc.py
+++
@@ -1,9 +0,0 @@
-import extractor
-
-
-
-data = extractor.extract_from_docx("sample_exam.docx")
-
-import json
-print(json.dumps(data, indent=2, ensure_ascii=False))
-
--- extractor_test_photo.py
+++
@@ -1,8 +0,0 @@
-import extractor
-
-
-with open("sample_exam.jpg", "rb") as f:
-    data = extractor.extract_from_images([(f.read(), "image/jpeg")])
-
-import json
-print(json.dumps(data, indent=2, ensure_ascii=False))
